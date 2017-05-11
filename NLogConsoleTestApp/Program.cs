359cf1707704b265edf085951ab12dac052727ec
Mike DePouw <mdepouw@ceiamerica.com>
2017-05-11 18:00:16 -0400

    -wire up nlog

--- NLogConsoleTestApp/Program.cs
+++ NLogConsoleTestApp/Program.cs
@@ -1,13 +1,41 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+using System;
 
 namespace NLogConsoleTestApp
 {
     class Program
     {
+        static ILogger<Program> logger;
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //https://andrewlock.net/using-dependency-injection-in-a-net-core-console-application/
+            //setup DI
+            var services = new ServiceCollection();
 
+            //add logging
+            services.AddLogging();
+
+            //build the provider;
+            var serviceProvider = services.BuildServiceProvider();
+
+            //configure console logging
+            serviceProvider
+                .GetService<ILoggerFactory>()
+                .AddConsole(LogLevel.Trace, true);
+
+            //configure nlog
+            var loggerFactory = serviceProvider.GetService<ILoggerFactory>();
+            loggerFactory.AddNLog();
+            loggerFactory.ConfigureNLog("nlog.config");
+
+            //create a logger
+            logger = serviceProvider.GetService<ILoggerFactory>()
+                .CreateLogger<Program>();
+
+            logger.LogInformation("Hello World!");
         }
     }
 }
